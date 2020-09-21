9e1137e630201bec9ef252735c982bdb372b1ce7
askwuxue <972523419@qq.com>
2020-09-21 22:59:30 +0800

    Create HTML folder and create Java files

---
+++ Java/HelloWorld/src/WorkArray.java
@@ -0,0 +1,8 @@
+public class WorkArray {
+    public static void main(String[] args) {
+        // TODO 求某年的最好成绩
+        // TODO 求某一年的平均成绩
+        // TODO 求所有年份的最好成绩
+        // TODO 求某门课历年的最好成绩
+    }
+}
