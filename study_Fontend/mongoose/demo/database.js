0ec1becccd6e8970200c485b660406f6a3db6e3f
askwuxue <972523419@qq.com>
2021-03-06 02:34:40 +0800

    update database and use validate

--- study_Fontend/mongoose/demo/database.js
+++ study_Fontend/mongoose/demo/database.js
@@ -29,11 +29,54 @@ const usersSchema = new Schema({
     password: String
 });
 
+// create postsSchema  user validate(验证)
+const postsSchema = new Schema({
+    title: {
+        type: String,
+        // require: true,
+        // minlength: 2,
+        // maxlength: 32,
+        required: [true, '该字段必须输入'],
+        minlength: [2, '该字段的最小长度为2'],
+        maxlength: [32, '该字段的最大长度为32']
+    },
+    date: {
+        type: Date,
+        // 如果没有赋值，会使用默认值
+        // TODO 使用Date.now 最终的日期不对 并不是当前的时间
+        default: Date.now
+    },
+    number: {
+        type: Number,
+        required: [true, '请输入一个整数'],
+        min: [1, '可输入的最小值为1'],
+        max: [9999, '可输入的最大值为9999']
+    },
+    category: {
+        type: String,
+        // input must include enum
+        enum: ['html', 'css', 'javascript', 'NodeJs']
+    },
+    author: {
+        type: String,
+        // 自定义验证 
+        validate: {
+            validator: v => {
+                // 长度大于4 返回布尔值 true: 验证成功
+                return v && v.length > 4;
+            }
+        }
+    }
+})
+
 // create CourseModel
 const Course = mongoose.model('Course', courseSchema);
 
 // create usersModel
 const Users = mongoose.model('Users', usersSchema);
+
+// create postsModel
+const Posts = mongoose.model('Posts', postsSchema);
 
 // create course document
 const course = new Course({
@@ -41,8 +84,6 @@ const course = new Course({
     author: 'wuxue',
     isPublished: false
 });
-
-// create users document
 
 
 // create new document
@@ -55,6 +96,15 @@ Course.create({ name: 'love You', author: 'xuning', isPublished: true })
     })
 
 course.save();
+
+// create new posts document
+Posts.create({ title: 'firstPost', number: 2, category: 'html', author: 'askwuxue' })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.log(err);
+    })
 
 // import database
 // C:\Program Files\MongoDB\Server\4.1\bin
