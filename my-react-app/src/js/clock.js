4f60ee6107013d5b37c9936d817c21ab7eb7122e
askwuxue <972523419@qq.com>
2021-04-05 16:31:08 +0800

    this test

--- my-react-app/src/js/clock.js
+++ my-react-app/src/js/clock.js
@@ -27,7 +27,7 @@
 class Clock extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {date: new Date()}
+        this.state = {date: new Date()};
     }
 
     componentDidMount() {
