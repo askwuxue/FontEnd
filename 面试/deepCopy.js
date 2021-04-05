76894d1175321cd55dc8f1bec5d52f325c7ed366
askwuxue <972523419@qq.com>
2021-04-05 22:46:30 +0800

    deepCopy filed

--- 面试/deepCopy.js
+++ 面试/deepCopy.js
@@ -1,5 +1,28 @@
-let obj = { name: 'wu', hobbies: ['a', 'b'] };
-
-function deepCopy() {
-
+function deepCopy(newObj, oldObj) {
+    for (let key in oldObj) {
+        console.log(oldObj[key]);
+        if (oldObj[key] instanceof Array) {
+            newObj[key] = [];
+            deepCopy(newObj[key], oldObj[key]);
+        } else if (oldObj[key] instanceof Object) {
+            newObj[key] = {};
+            deepCopy(newObj[key], oldObj[key]);
+        }
+        newObj[key] = oldObj[key];
+    }
+    return newObj;
 }
+
+let oldObj = { name: 'wu', hobbies: ['a', 'b'], json: {a: "1", b: "2"} };
+
+let newObj = {};
+
+// deepCopy(newObj, oldObj); 
+// console.log('deepCopy(newObj, oldObj): ', deepCopy(newObj, oldObj));
+
+let target = deepCopy(newObj, oldObj);
+
+target.json.b = "3";
+
+console.log('oldObj: ', oldObj);
+
