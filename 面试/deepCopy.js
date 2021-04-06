099d24e2190c44f53f18db56cfe8d12fd1e4265f
askwuxue <972523419@qq.com>
2021-04-06 23:51:25 +0800

    update deepCopy

--- 面试/deepCopy.js
+++ 面试/deepCopy.js
@@ -1,28 +1,58 @@
-function deepCopy(newObj, oldObj) {
-    for (let key in oldObj) {
-        console.log(oldObj[key]);
-        if (oldObj[key] instanceof Array) {
-            newObj[key] = [];
-            deepCopy(newObj[key], oldObj[key]);
-        } else if (oldObj[key] instanceof Object) {
-            newObj[key] = {};
-            deepCopy(newObj[key], oldObj[key]);
+// TODO 下面注释的代码功能实现了，但是不是深拷贝
+// function deepCopy(newObj, oldObj) {
+//     for (let key in oldObj) {
+//         console.log(oldObj[key]);
+//         if (oldObj[key] instanceof Array) {
+//             newObj[key] = [];
+//             deepCopy(newObj[key], oldObj[key]);
+//         } else if (oldObj[key] instanceof Object) {
+//             newObj[key] = {};
+//             deepCopy(newObj[key], oldObj[key]);
+//         }
+//         newObj[key] = oldObj[key];
+//     }
+//     return newObj;
+// }
+
+// let oldObj = { name: 'wu', hobbies: ['a', 'b'], json: {a: "1", b: "2"} };
+
+// let newObj = {};
+
+// // deepCopy(newObj, oldObj); 
+// // console.log('deepCopy(newObj, oldObj): ', deepCopy(newObj, oldObj));
+
+// let target = deepCopy(newObj, oldObj);
+
+// target.json.b = "3";
+
+// console.log('oldObj: ', oldObj);
+
+function deepCopy(obj) {
+    
+    // 如果传递的参数不是引用数组类型，直接返回
+    if (typeof obj !== 'object') return obj;
+ 
+    const newObj = Array.isArray(obj) ? [] : {};
+
+    for (let key in obj) {
+
+        // 如果key是obj实例上的属性
+        if (obj.hasOwnProperty(key)) {
+
+            // 如果当前属性的属性值为引用类型，则递归，否则直接赋值
+            newObj[key] = typeof obj[key] === 'object' ? deepCopy(obj[key]) : obj[key];
+        
         }
-        newObj[key] = oldObj[key];
+
     }
+
     return newObj;
 }
 
 let oldObj = { name: 'wu', hobbies: ['a', 'b'], json: {a: "1", b: "2"} };
 
-let newObj = {};
+let newObj = deepCopy(oldObj);
 
-// deepCopy(newObj, oldObj); 
-// console.log('deepCopy(newObj, oldObj): ', deepCopy(newObj, oldObj));
+newObj.json.a = '3';
 
-let target = deepCopy(newObj, oldObj);
-
-target.json.b = "3";
-
-console.log('oldObj: ', oldObj);
-
+console.log(oldObj);
