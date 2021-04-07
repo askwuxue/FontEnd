f02a8e6d69c5506f28076893020690f0f4e498b8
askwuxue <972523419@qq.com>
2021-04-07 21:34:51 +0800

    upgrade

--- 面试/render.js
+++ 面试/render.js
@@ -15,7 +15,7 @@ function render(template, context) {
 // V1.0
 function template(str, obj) {
     // 第二个参数为function 返回值会替换匹配的子串 match为匹配到的子串 key相当于$n
-    return str.replace(/\{\{(.*?)\}\}/g, (match, key) => obj[key.trim()])
+    return str.replace(/\{\{(.*?)\}\}/g, (match, key) => obj[key.trim()]);
 }
 
 console.log(template(str, obj));
