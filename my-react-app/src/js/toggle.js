d198d96dde57c9748b8535279ea4fd63392b0abd
askwuxue <972523419@qq.com>
2021-04-09 22:34:44 +0800

    update TODO list is class' function' this

--- my-react-app/src/js/toggle.js
+++ my-react-app/src/js/toggle.js
@@ -2,7 +2,8 @@ class Toggle extends React.Component {
     constructor(props) {
         super(props);
         this.state = {isToggleOn: true};
-        // TODO 必须显式的绑定this给handleClick
+        // TODO 必须显式的绑定this给handleClick 
+        // TODO 因为这个类中的方法中的this会被React指向undefined，而不是方法的调用者，所以显示绑定或者箭头函数
         // this.handleClick = this.handleClick.bind(this);
     }
 
