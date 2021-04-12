c0be35d114a25a6a1461a51f7f984fea6b886f65
askwuxue <972523419@qq.com>
2021-04-12 22:02:51 +0800

    decorators-legacy' isn't currently enabled git statusgit status

--- React/lesson1/config-overrides.js
+++
@@ -1,8 +0,0 @@
-const {override, addDecoratorsLegacy} = require("customize-cra");
-
-module.exports = function override(config, env) {
-    // do stuff with the webpack config...
-    // return config;
-    addDecoratorsLegacy();
-    return config;
-  };
