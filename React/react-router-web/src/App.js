63bb7c76a97604c00c7fb03098ac5f77a5c9c7ce
askwuxue <972523419@qq.com>
2021-04-21 21:09:54 +0800

    react-router-dom use

---
+++ React/react-router-web/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header'
+import Nav from './components/Nav'
+// import Home from './pages/Home'
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Nav />
+      {/* <Home /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ React/react-router-web/src/components/Nav.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { NavLink, Route } from 'react-router-dom'
+import About from '../pages/About'
+import Home from '../pages/Home'
+
+export default class Nav extends Component {
+    render() {
+        return (
+            <div>
+                <nav className='nav'>
+                    {/* Link 其实最终会被渲染成a标签 */}
+                    <NavLink to="/home" className="btn btn-default" activeClassName="hightLight">Home</NavLink>
+                    {/* <Link to='/home' className="btn btn-default">Home</Link> */}
+                {/* <a className="btn btn-default" href="/home" role="button">Home</a> */}
+                {/* <a className="btn btn-default" href="/about" role="button">About</a> */}
+                     {/* <Link to='/about' className="btn btn-default">About</Link> */}
+                     <NavLink to="/home" className="btn btn-default"  activeClassName="hightLight">About</NavLink>
+                     <Route path='/home' component={ Home }/>
+                     <Route path='/about' component={ About }/>
+                </nav>
+            </div>
+        )
+    }
+}
