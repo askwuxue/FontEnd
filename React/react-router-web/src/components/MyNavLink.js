f677a0c4735e0b0741915cbb5a5c4f86c4851c05
askwuxue <972523419@qq.com>
2021-04-21 21:55:12 +0800

    NavLink packaging to MyNavLink component

---
+++ React/react-router-web/src/components/MyNavLink.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import {NavLink} from 'react-router-dom'
+
+export default class MyNavLink extends Component {
+    render() {
+        return (
+            <div>
+                {/* TODO2 注 {...this.props这种写法是将props一一对应进行传递}
+                    this.props.children属性最终也会渲染在标签体中
+                    {this.props.children} 不用写咋标签体里面，直接放在属性中
+                    渲染的时候会被渲染成和在标签体中一样的
+                */}
+                <NavLink {...this.props} activeClassName="highLight" className="list-group-item" ></NavLink>
+            </div>
+        )
+    }
+}
--- React/react-router-web/src/components/Nav.js
+++ React/react-router-web/src/components/Nav.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { NavLink, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import About from '../pages/About'
 import Home from '../pages/Home'
+import MyNavLink from './MyNavLink'
 
 export default class Nav extends Component {
     render() {
@@ -9,17 +10,22 @@ export default class Nav extends Component {
             <div>
                 <nav className='nav'>
                     {/* Link 其实最终会被渲染成a标签 */}
-                    {/* TODO NavLink 的activeClassName为什么对应的css属性会在页面加载就被渲染
+                    {/* TODO1 NavLink 的activeClassName为什么对应的css属性会在页面加载就被渲染
                         而不是在点击选中状态的时候被选中*/}
-                    <NavLink to="/home" activeClassName="highLight" className="list-group-item" >Home</NavLink>
                     {/* <Link to='/home' className="btn btn-default">Home</Link> */}
-                {/* <a className="btn btn-default" href="/home" role="button">Home</a> */}
-                {/* <a className="btn btn-default" href="/about" role="button">About</a> */}
-                     {/* <Link to='/about' className="btn btn-default">About</Link> */}
-                     <NavLink to="/home" activeClassName="highLight" className="list-group-item" >About</NavLink>
-                     {/* 路由组件接受的默认接受到props history location match */}
-                     <Route path='/home' component={ Home }/>
-                     <Route path='/about' component={ About }/>
+                    {/* <a className="btn btn-default" href="/home" role="button">Home</a> */}
+                    {/* <a className="btn btn-default" href="/about" role="button">About</a> */}
+                    {/* <Link to='/about' className="btn btn-default">About</Link> */}
+                     
+                    {/* <NavLink to="/home" activeClassName="highLight" className="list-group-item" >Home</NavLink>
+                    <NavLink to="/home" activeClassName="highLight" className="list-group-item" >About</NavLink> */}
+                    
+                    {/* 封装的MyNavLink的使用 */}
+                    <MyNavLink to="/home">Home</MyNavLink>
+                    <MyNavLink to="/about">About</MyNavLink>
+                    {/* 路由组件接受的默认接受到props history location match */}
+                    <Route path='/home' component={ Home }/>
+                    <Route path='/about' component={ About }/>
                 </nav>
             </div>
         )
