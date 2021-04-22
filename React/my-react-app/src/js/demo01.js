2cc1837ca127bb5158cad50f91fdd7febdfa8686
askwuxue <972523419@qq.com>
2021-04-23 00:19:00 +0800

    test

--- React/my-react-app/src/js/demo01.js
+++ React/my-react-app/src/js/demo01.js
@@ -1,4 +1,27 @@
+// import React, { Component } from 'react'
+class Demo01 extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.test();
+    }
+
+    test() {
+        console.log('test this is: ')
+        console.log(this);
+    }
+
+
+    render() {
+        return (
+            <div>
+                this is app...
+            </div>
+        )
+    }
+}
+
 ReactDOM.render(
-    <h1>Hello world</h1>,
+    <Demo01 />,
     document.getElementById('root')
 );
