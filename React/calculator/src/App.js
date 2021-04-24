d7973d6e5bb38a619c3e7e793f56156011c18bec
askwuxue <972523419@qq.com>
2021-04-24 18:07:54 +0800

    use Provider

--- React/calculator/src/App.js
+++ React/calculator/src/App.js
@@ -1,11 +1,11 @@
 import Count from './containers/Count/Count'
-import store from './redux/store'
+// import store from './redux/store'
 
 function App() {
   return (
     <div>
-      {/* TODO必须将store传递给容器组件 */}
-      <Count store={store}></Count>
+      {/* TODO必须将store传递给容器组件 Provider代理也可以 */}
+      <Count></Count>
     </div>
   );
 }
--- React/calculator/src/index.js
+++ React/calculator/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux'
+import store  from './redux/store'
 
 ReactDOM.render(
-    <App/>,
+  // 将所有组件包括在Provider中，所有使用store的容器组件都会被传递store
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );
