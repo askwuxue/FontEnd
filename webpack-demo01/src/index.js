656e85e13b8f54b8675096a5eb00f7fd436b5380
askwuxue <972523419@qq.com>
2021-04-25 21:18:49 +0800

    webpack install and first use

---
+++ webpack-demo01/src/index.js
@@ -0,0 +1,16 @@
+// webpack的入口文件 /src/index.js
+import test from './app'
+
+// 开发环境打包
+// webpack .\src\index.js -o .\build\build.js --mode=development
+
+// 线上环境打包 会将入口文件压缩
+// webpack .\src\index.js -o .\build\build.js --mode=production
+
+// webpack 默认只支持打包.js .json 对与.css img 等资源不支持，需要借助loader和plugins实现
+import data from '../public/data.json';
+// import '../public/app.css'
+
+const { a, b } = data;
+
+test(a, b);
