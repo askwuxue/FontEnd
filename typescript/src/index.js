b7486c553d198db40a1ab1c3bc068f883673858a
askwuxue <972523419@qq.com>
2021-05-10 23:42:08 +0800

    update

---
+++ typescript/src/index.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var c_1 = require("./module/c");
+var a = 1;
+function demo(num) {
+    console.log(num);
+    console.log(num + 'demo1');
+    console.log(c_1.c);
+}
+demo(5);
