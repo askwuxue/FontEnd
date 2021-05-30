462cc0b9cc6795a3485d0ad00cff0caca91a04b2
askwuxue <972523419@qq.com>
2021-05-30 12:06:56 +0800

    configurate clean before build, devServe, resolve's extensions

--- typescript/webpack/webpack.config.js
+++ typescript/webpack/webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js'
+        filename: 'bundle.js',
+        // 代替clean-webpack-plugin 编译前先清除目录
+        clean: true
     },
     module: {
         rules: [
@@ -14,5 +17,20 @@ module.exports = {
             }
         ]
     },
+    // 配置别名，引入模块的格式等，参考官网
+    resolve: {
+        // webpack默认只支持js模块，不支持ts模块的引入，通过extensions配置可以引入模块的类型
+        extensions: ['.js', '.json', '.ts']
+    },
+    // 配置webpack-dev-server，自动打开浏览器热更新。package.json中也进行配置
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        // 自动生成或者使用已有的html模板，将script自动引入
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ],
     mode: 'development'
 }
