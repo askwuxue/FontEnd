5bc259109f10edf130ac0391228bec56a13ee4ae
askwuxue <972523419@qq.com>
2021-06-06 10:55:58 +0800

    update virtual generate text node

--- React/tiny-react/src/TinyReact/creatElement.js
+++ React/tiny-react/src/TinyReact/creatElement.js
@@ -1,7 +1,14 @@
-export default function createElement(type, props, children) {
+export default function createElement(type, props, ...children) {
+    const textElement = [...children].map(child => {
+        if (child instanceof Object) {
+            return child;
+        } else {
+            return createElement('text', { textContent: child })
+        }
+    })
     return {
         type,
         props,
-        children
+        children: textElement
     }
 }
