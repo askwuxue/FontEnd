fd7bddb3b7d1d38ff283596159e7615bf9dc405f
askwuxue <972523419@qq.com>
2021-06-08 00:14:25 +0800

    update oldDOM is containner.firstChild

--- React/tiny-react/src/TinyReact/render.js
+++ React/tiny-react/src/TinyReact/render.js
@@ -3,9 +3,9 @@ import diff from './diff';
 /* 
 virtualDom: JSX，函数组件或类组件
 container: 页面容器位置
-oldDom: 旧VirtualDOM
+oldDOM: 旧VirtualDOM 
 */
-export default function render(virtualDom, container, oldDom) {
+export default function render(virtualDom, container, oldDOM = container.firstChild) {
     // 渲染之前，新旧VirtualDOM要进行比较
-    diff(virtualDom, container, oldDom);
+    diff(virtualDom, container, oldDOM);
 }
