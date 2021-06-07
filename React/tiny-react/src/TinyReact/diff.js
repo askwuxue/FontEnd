73978c53bf62167ec7281c80bc47ca00e870ea53
askwuxue <972523419@qq.com>
2021-06-08 00:00:35 +0800

    update diff function

--- React/tiny-react/src/TinyReact/diff.js
+++ React/tiny-react/src/TinyReact/diff.js
@@ -1,11 +1,29 @@
 import mountElement from './mountElement';
+import updateNodeElement from './updateNodeElement';
+import updateTextNode from './updateTextNode';
 
-export default function diff(virtualDom, container, oldDom) {
-    // 判断oldDOM 是否存在
-    if (!oldDom) {
+export default function diff(virtualDOM, container, oldDOM) {
+    // 旧virtualDOM,挂载在DOM元素的属性上
+    const oldVirtualDOM = oldDOM && oldDOM._virtualDOM;
+    // 首次渲染
+    if (!oldDOM) {
         // 挂载元素
-        mountElement(virtualDom, container)
-    } else {
+        mountElement(virtualDOM, container)
+        // 更新DOM元素
+        // 类型相同
+    } else if (oldVirtualDOM && oldVirtualDOM.type === virtualDOM.type) {
+        // 文本类型节点
+        if (virtualDOM.type === 'text') {
+            // 更新文本类型节点内容
+            updateTextNode(oldVirtualDOM, virtualDOM, oldDOM);
+            // 更新节点属性
+        } else {
+            updateNodeElement(oldDOM, virtualDOM, oldVirtualDOM);
+        }
 
+        // 更新子元素
+        virtualDOM.children.forEach((child, index) => {
+            diff(child, oldDOM, oldDOM.childNodes[index]);
+        })
     }
 }
