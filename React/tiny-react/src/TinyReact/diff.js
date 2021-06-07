85b04b3f5ea11cd107ac0a72375697baad81c24c
askwuxue <972523419@qq.com>
2021-06-08 00:11:17 +0800

    update element

--- React/tiny-react/src/TinyReact/diff.js
+++ React/tiny-react/src/TinyReact/diff.js
@@ -1,3 +1,4 @@
+import createDOMElement from './createDOMElement';
 import mountElement from './mountElement';
 import updateNodeElement from './updateNodeElement';
 import updateTextNode from './updateTextNode';
@@ -10,6 +11,12 @@ export default function diff(virtualDOM, container, oldDOM) {
         // 挂载元素
         mountElement(virtualDOM, container)
         // 更新DOM元素
+        // 类型不同
+    } else if (virtualDOM.type !== oldVirtualDOM.type && virtualDOM.type !== 'function') {
+        // 新的DOM元素
+        const newElement = createDOMElement(virtualDOM);
+        // 节点替换
+        oldDOM.parentNode.replaceChild(newElement, oldDOM);
         // 类型相同
     } else if (oldVirtualDOM && oldVirtualDOM.type === virtualDOM.type) {
         // 文本类型节点
