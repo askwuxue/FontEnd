72c8d46d5208bbb4cee879828bfcb64863a6d3dc
askwuxue <972523419@qq.com>
2021-06-09 23:12:32 +0800

    add logic at oldDOM exist

--- React/tiny-react/src/TinyReact/mountNativeElement.js
+++ React/tiny-react/src/TinyReact/mountNativeElement.js
@@ -1,7 +1,18 @@
 import createDOMElement from './createDOMElement';
+import unmountNode from './unmountNode';
 
-export default function mountNativeElement(virtualDom, container) {
-    let newElement = createDOMElement(virtualDom);
+export default function mountNativeElement(virtualDOM, container, oldDOM) {
+    // 已经存在旧的DOM元素，先删除，后渲染
+    if (oldDOM) {
+        unmountNode(oldDOM);
+    }
+    let newElement = createDOMElement(virtualDOM);
     // 将创建好的元素放入container
     container.append(newElement);
+    // 取组件实例
+    let component = virtualDOM.component;
+    if (component) {
+        // 实例对象调用setDOM,实际会调用父类上的setDOM
+        component.setDOM(newElement);
+    }
 }
