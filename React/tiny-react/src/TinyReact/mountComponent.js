f1ad94fe6468fd82ea4d133e3b0b4725ed287448
askwuxue <972523419@qq.com>
2021-06-09 23:10:55 +0800

    mountComponent and mountNativeElement append second param

--- React/tiny-react/src/TinyReact/mountComponent.js
+++ React/tiny-react/src/TinyReact/mountComponent.js
@@ -2,40 +2,42 @@ import isFunction from "./isFunction";
 import isFunctionComponent from "./isFunctionComponent";
 import mountNativeElement from './mountNativeElement';
 
-export default function mountComponent(virtualDom, container) {
+export default function mountComponent(virtualDOM, container, oldDOM) {
     let nextVirtualDOM = null;
     // 1. 函数组件
-    if (isFunctionComponent(virtualDom)) {
-        nextVirtualDOM = buildFunctionComponent(virtualDom);
+    if (isFunctionComponent(virtualDOM)) {
+        nextVirtualDOM = buildFunctionComponent(virtualDOM);
         // 组件中返回了另一个组件
         if (isFunction(nextVirtualDOM)) {
-            mountComponent(nextVirtualDOM, container);
+            mountComponent(nextVirtualDOM, container, oldDOM);
         } else {
-            mountNativeElement(nextVirtualDOM, container);
+            mountNativeElement(nextVirtualDOM, container, oldDOM);
         }
         // 2. 类组件
     } else {
-        nextVirtualDOM = buildClassComponent(virtualDom);
+        nextVirtualDOM = buildClassComponent(virtualDOM);
         // 返回了另一个组件
         if (isFunction(nextVirtualDOM)) {
-            mountComponent(nextVirtualDOM, container);
+            mountComponent(nextVirtualDOM, container, oldDOM);
         } else {
-            mountNativeElement(nextVirtualDOM, container);
+            mountNativeElement(nextVirtualDOM, container, oldDOM);
         }
     }
 }
 
 // 编译函数组件
-function buildFunctionComponent(virtualDom) {
+function buildFunctionComponent(virtualDOM) {
     // 为组件添加props
-    return virtualDom.type(virtualDom.props || {});
+    return virtualDOM.type(virtualDOM.props || {});
 }
 
 // 编译类组件
-function buildClassComponent(virtualDom) {
+function buildClassComponent(virtualDOM) {
     // 创建实例 将props传递给类组件
-    const component = new virtualDom.type(virtualDom.props);
+    const component = new virtualDOM.type(virtualDOM.props);
     // 渲染DOM
     const nextVirtualDOM = component.render();
+    // 添加组件实例到virtualDOM
+    nextVirtualDOM.component = component;
     return nextVirtualDOM;
 }
