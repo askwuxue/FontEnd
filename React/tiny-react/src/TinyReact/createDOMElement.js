89e2eb6bb06b3816187af6b12a765abb96bda956
askwuxue <972523419@qq.com>
2021-06-11 22:54:10 +0800

    virturlDOM.chidren is not virturlDOM is bug

--- React/tiny-react/src/TinyReact/createDOMElement.js
+++ React/tiny-react/src/TinyReact/createDOMElement.js
@@ -2,13 +2,15 @@ import mountElement from './mountElement';
 import updateNodeElement from './updateNodeElement';
 
 export default function createDOMElement(virtualDOM) {
+    console.log('virtualDOM: ', virtualDOM);
+
     // 生成真实DOM
     let newElement = null;
-    // 1. 文本节点
     if (virtualDOM.type === 'text') {
+        // 1. 文本节点
         newElement = document.createTextNode(virtualDOM.props.textContent);
     } else {
-        // console.log(virtualDOM.type);
+        // 元素节点
         newElement = document.createElement(virtualDOM.type);
         // 为元素添加属性
         updateNodeElement(newElement, virtualDOM);
@@ -18,9 +20,27 @@ export default function createDOMElement(virtualDOM) {
     newElement._virtualDOM = virtualDOM
 
     // 递归创建
-    virtualDOM.children.forEach(child => {
-        mountElement(child, newElement);
-    });
+    // virtualDOM.children?.forEach(child => {
+    //     mountElement(child, newElement);
+    // });
+    if (Array.isArray(virtualDOM)) {
+        virtualDOM.forEach(child => {
+            mountElement(child, newElement);
+        });
+    }
+
+    // TODO 在JSX中使用map 动态渲染，返回了数组，需要特别处理
+    if (Array.isArray(virtualDOM.children)) {
+        virtualDOM.children.forEach(child => {
+            mountElement(child, newElement);
+        });
+    }
+
+    // 元素的ref属性处理
+    if (virtualDOM.props && virtualDOM.props.ref) {
+        // ref函数接受参数为当前元素
+        virtualDOM.props.ref(newElement);
+    }
 
     return newElement;
 }
