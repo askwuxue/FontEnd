d0ca488e7238912a56b2e32c65e16d5dd487422d
askwuxue <972523419@qq.com>
2021-08-01 22:07:00 +0800

    multi then function call

--- JavaScript/Promise/MyPromise.js
+++ JavaScript/Promise/MyPromise.js
@@ -16,7 +16,11 @@ const resolvePromise = (promise, nextValue, resolve, reject) => {
 class MyPromise {
     // Promise 对象接受一个执行器，会立即执行
     constructor(executor) {
-        executor(this.resolve, this.reject)
+        try {
+            executor(this.resolve, this.reject)
+        } catch (e) {
+            this.reject(e)
+        }
     }
 
     // Promise的状态
@@ -42,7 +46,7 @@ class MyPromise {
         this.value = value;
         // 成功回调函数的执行。此时resolve函数已经执行
         // this.successCallBack && this.successCallBack(value);
-        while (this.successCallBack.length) this.successCallBack.shift()(value);
+        while (this.successCallBack.length) this.successCallBack.shift()();
     }
 
     // reject接受失败的原因
@@ -53,30 +57,74 @@ class MyPromise {
         this.reason = reason;
         // 失败回调函数的执行。此时reject函数已经执行
         // this.failCallBack && this.failCallBack(reason);
-        while (this.failCallBack.length) this.failCallBack.shift()(reason);
+        while (this.failCallBack.length) this.failCallBack.shift()();
     }
 
     // 执行Promise对象的then方法
     then = (successCallBack, failCallBack) => {
+        // 如果then方法没有传递参数，则使用一个函数作为默认参数
+        successCallBack = successCallBack ? successCallBack : value => value
+        failCallBack = failCallBack ? failCallBack : reason => { throw reason }
         // then方法返回一个Promise对象
         let promise = new MyPromise((resolve, reject) => {
             // 当前状态是fulfilled，执行成功回调
             if (this.status === FULFILLED) {
                 // 为了能拿到Mypromise实例对象，需要异步执行函数
                 setTimeout(() => {
-                    // 传递给下一个Promise对象的值是then方法的返回值，成功时即successCallBack的值
-                    let nextValue = successCallBack(this.value);
-                    // 判断上一个Promise对象的then方法返回的值的类型，如果是普通值，直接调用resolve方法
-                    // 如果是Promise对象，先判断Promise对象的状态，成功调用resolve方法，失败调用reject
-                    resolvePromise(promise, nextValue, resolve, reject);
+                    try {
+                        // 传递给下一个Promise对象的值是then方法的返回值，成功时即successCallBack的值
+                        let nextValue = successCallBack(this.value);
+                        // 判断上一个Promise对象的then方法返回的值的类型，如果是普通值，直接调用resolve方法
+                        // 如果是Promise对象，先判断Promise对象的状态，成功调用resolve方法，失败调用reject
+                        resolvePromise(promise, nextValue, resolve, reject);
+                    } catch (e) {
+                        reject(e)
+                    }
                 }, 0)
                 // 当前状态是rejected，执行失败回调
             } else if (this.status === REJECTED) {
-                failCallBack(this.reason);
+                // 为了能拿到Mypromise实例对象，需要异步执行函数
+                setTimeout(() => {
+                    try {
+                        // 传递给下一个Promise对象的值是then方法的返回值，成功时即successCallBack的值
+                        let nextValue = failCallBack(this.reason);
+                        // 判断上一个Promise对象的then方法返回的值的类型，如果是普通值，直接调用resolve方法
+                        // 如果是Promise对象，先判断Promise对象的状态，成功调用resolve方法，失败调用reject
+                        resolvePromise(promise, nextValue, resolve, reject);
+                    } catch (e) {
+                        reject(e)
+                    }
+                }, 0)
                 // 当前状态是padding, 如异步函数调用。此时resolve和reject都没有调用。暂时将成功和失败的回调存储。
             } else {
-                this.successCallBack.push(successCallBack)
-                this.failCallBack.push(failCallBack)
+                this.successCallBack.push(() => {
+                    // 为了能拿到Mypromise实例对象，需要异步执行函数
+                    setTimeout(() => {
+                        try {
+                            // 传递给下一个Promise对象的值是then方法的返回值，成功时即successCallBack的值
+                            let nextValue = successCallBack(this.value);
+                            // 判断上一个Promise对象的then方法返回的值的类型，如果是普通值，直接调用resolve方法
+                            // 如果是Promise对象，先判断Promise对象的状态，成功调用resolve方法，失败调用reject
+                            resolvePromise(promise, nextValue, resolve, reject);
+                        } catch (e) {
+                            reject(e)
+                        }
+                    }, 0)
+                })
+                this.failCallBack.push(() => {
+                    // 为了能拿到Mypromise实例对象，需要异步执行函数
+                    setTimeout(() => {
+                        try {
+                            // 传递给下一个Promise对象的值是then方法的返回值，成功时即successCallBack的值
+                            let nextValue = failCallBack(this.reason);
+                            // 判断上一个Promise对象的then方法返回的值的类型，如果是普通值，直接调用resolve方法
+                            // 如果是Promise对象，先判断Promise对象的状态，成功调用resolve方法，失败调用reject
+                            resolvePromise(promise, nextValue, resolve, reject);
+                        } catch (e) {
+                            reject(e)
+                        }
+                    }, 0)
+                })
             }
         })
         return promise;
--- JavaScript/Promise/promise.js
+++ JavaScript/Promise/promise.js
@@ -1,28 +1,39 @@
 const MyPromise = require('./MyPromise');
 
 let promise = new MyPromise((resolve, reject) => {
-    resolve('成功');
+    // resolve('成功');
+    // throw new Error('executor error');
     // setTimeout(() => {
     //     resolve('成功');
     // }, 2000);
-    // reject('失败的原因');
+    reject('失败的原因');
 })
 
-const others = new MyPromise((resolve, reject) => {
-    resolve('others Promise')
+promise.then().then().then(value => {
+    console.log(value)
+}, err => {
+    console.log(err);
 })
 
-let p1 = promise.then(value => {
-    console.log(value)
-    // console.log(value)
-    // return 100;
-    return p1
-})
-p1.then((value) => {
-    console.log(value)
-}, reason => {
-    console.log(reason.message)
-})
+// const others = new MyPromise((resolve, reject) => {
+//     resolve('others Promise')
+// })
+
+// let p1 = promise.then(value => {
+//     console.log(value)
+//     // throw new Error('then error')
+//     // return 100;
+//     // return p1
+//     return 'aaa';
+// }, (error) => {
+//     console.log(error);
+//     return 10000;
+// })
+// p1.then((value) => {
+//     console.log(value)
+// }, reason => {
+//     console.log(reason.message)
+// })
 // p1.then(value => {
 //      console.log(value)
 //      return others;
